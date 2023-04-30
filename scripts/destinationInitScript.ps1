6c0d4c2a95cd72d31b6cb7c1e046810c51c839e0
James <jgodden90@outlook.com>
2023-04-30 18:28:51 -0400

    testing

--- scripts/destinationInitScript.ps1
+++ scripts/destinationInitScript.ps1
@@ -19,4 +19,4 @@ New-WebBinding -Name $siteName -Port $port -Protocol "https"
 
 $SSLCert = Get-ChildItem –Path "cert:\LocalMachine\My" | Where-Object {$_.subject -like 'cn=localhost'}
 Set-Location "IIS:\sslbindings"
-New-Item '!10001!' -value $SSLCert
+New-Item !10001! -value $SSLCert
