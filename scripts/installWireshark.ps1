9c5579970bd8e9a683b3ae4dce61422616445a92
jimgodden <jgodden90@outlook.com>
2023-05-03 15:33:42 -0400

    removing etl2pcapng

--- scripts/installWireshark.ps1
+++ scripts/installWireshark.ps1
@@ -1,7 +1,7 @@
 $wshell = New-Object -ComObject Wscript.Shell
 $answer = $wshell.Popup("Do you want to download Wireshark?",0,"Download Option",32+4)
 if ($answer -eq 6) {
-    Install-Module Convert-Etl2Pcapng -Scope CurrentUser -Force
+    # Install-Module Convert-Etl2Pcapng -Scope CurrentUser -Force
     Write-Host "Downloading Wireshark to capture packets and catch the next occurrance.."
     Invoke-WebRequest -Uri https://2.na.dl.wireshark.org/win64/Wireshark-win64-3.6.13.exe -OutFile Wireshark-win64-3.6.13.exe
     .\Wireshark-win64-3.6.13.exe
